c60aa6dd81f2ec958b8218e62acc043f09e44685
gthomp22 <thompson.g1@stu.janesville.k12.wi.us>
2024-12-16 14:41:13 -0600

    12/16

--- Pg153/Pg153/MainForm.py
+++ Pg153/Pg153/MainForm.py
@@ -67,36 +67,40 @@ class MainForm(Form):
         # 
         # button1
         # 
+        self._button1.BackColor = System.Drawing.SystemColors.ControlLight
         self._button1.Location = System.Drawing.Point(12, 167)
         self._button1.Name = "button1"
         self._button1.Size = System.Drawing.Size(90, 32)
         self._button1.TabIndex = 6
         self._button1.Text = "Calculate"
-        self._button1.UseVisualStyleBackColor = True
+        self._button1.UseVisualStyleBackColor = False
         self._button1.Click += self.Button1Click
         # 
         # button2
         # 
+        self._button2.BackColor = System.Drawing.SystemColors.ControlLight
         self._button2.Location = System.Drawing.Point(141, 167)
         self._button2.Name = "button2"
         self._button2.Size = System.Drawing.Size(90, 32)
         self._button2.TabIndex = 7
         self._button2.Text = "Clear"
-        self._button2.UseVisualStyleBackColor = True
+        self._button2.UseVisualStyleBackColor = False
         self._button2.Click += self.Button2Click
         # 
         # button3
         # 
+        self._button3.BackColor = System.Drawing.SystemColors.ControlLight
         self._button3.Location = System.Drawing.Point(78, 205)
         self._button3.Name = "button3"
         self._button3.Size = System.Drawing.Size(90, 32)
         self._button3.TabIndex = 8
         self._button3.Text = "Exit"
-        self._button3.UseVisualStyleBackColor = True
+        self._button3.UseVisualStyleBackColor = False
         self._button3.Click += self.Button3Click
         # 
         # MainForm
         # 
+        self.BackColor = System.Drawing.SystemColors.AppWorkspace
         self.ClientSize = System.Drawing.Size(243, 246)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
